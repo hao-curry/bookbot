e22c5110031b3f527399f09cb78adb970d4254c8
NoRacket <ronmaecus@gmail.com>
2026-06-28 21:47:29 +0200

    main now prints the content of frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(path: str) -> str:
+    with open(path) as file:
+        return file.read()
+
+
+def main() -> None:
+    print(get_book_text("books/frankenstein.txt"))
+
+
+if __name__ == '__main__':
+    main()
